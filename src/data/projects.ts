70113269a85653031be20723ceb7caff8d17aa7c
MaxMateus <m-maxmateus@hotmail.com>
2026-01-12 19:43:34 -0300

    removendo projeto

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -12,22 +12,22 @@ export type Project = {
 }
 
 export const projects: Project[] = [
-  {
-    id: 'ms-auth',
-    title: 'ms-auth',
-    year: '2025',
-    description:
-      'Serviço de autenticação em Laravel com suporte a MFA (e-mail, SMS e WhatsApp) e emissão de tokens via Passport.',
-    link: 'https://github.com/maxmateus/ms-auth',
-    status: 'Concluído',
-    progress: 100,
-    techs: ['Laravel', 'PHP', 'Docker', 'MySQL', 'Redis', 'Swagger'],
-    featured: true,
-  },
+  // {
+  //   id: 'ms-auth',
+  //   title: 'ms-auth',
+  //   year: '2025',
+  //   description:
+  //     'Serviço de autenticação em Laravel com suporte a MFA (e-mail, SMS e WhatsApp) e emissão de tokens via Passport.',
+  //   link: 'https://github.com/maxmateus/ms-auth',
+  //   status: 'Concluído',
+  //   progress: 100,
+  //   techs: ['Laravel', 'PHP', 'Docker', 'MySQL', 'Redis', 'Swagger'],
+  //   featured: true,
+  // },
   {
     id: 'minibank',
     title: 'Bank',
-    year: '2025',
+    year: '2026',
     description:
       'Banco digital para estudo, com arquitetura em camadas e responsabilidades bem definidas. O projeto simula fluxos centrais de um sistema bancário, envolvendo controle de acesso, gestão de contas, movimentações financeiras com regras de negócio e integração entre frontend e backend.',
     repos: [
